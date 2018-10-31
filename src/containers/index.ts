326cbca1e793eacf60d469a83977332d0e5ff609
LengYXin <896630361@qq.com>
2018-10-31 15:06:09 +0800

    update

--- src/containers/index.ts
+++ src/containers/index.ts
@@ -1,7 +1,6 @@
 export default {
     editer: () => import('./editer').then(x => x.default),
     /**WTM**/ 
-    test: () => import('./test').then(x => x.default),
-    testa: () => import('./testa').then(x => x.default)
+  
     /**WTM**/
 }
